0df6f21851aa6a824af4790e5bdcbb2cf9863120
Jean-Paul Ainam <jpainam@gmail.com>
2024-07-15 12:39:33 -0400

    Add utility function for merging class names

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export function Header() {
+  return (
+    <div className="flex flex-col gap-8">
+      <h1>Header</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
